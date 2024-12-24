35266d04b3195993baadeadeab5fff6f993798a2
kadir-zeren <k4dzeren@gmail.com>
2024-12-24 11:15:27 +0300

    practice

---
+++ python.py
@@ -0,0 +1,92 @@
+print('clarusway')
+print('3+5')
+print ('abc')
+print(' !-** asdflkasdjlfk')
+
+a,b = 3, 5
+print(a,b)
+
+r = 5
+pi = 3.14
+area = pi * r ** 2
+print(area)
+
+r = 5
+pi = 3.14
+area = pi * (r ** 2) 
+print(area) 
+
+a = 5
+a = a + 1
+a
+
+a +=1
+a
+
+A = 10
+A = A * 2
+A
+
+A *= 2
+A
+
+a = 3
+b = 4
+hipotenus = (a ** 2 + b ** 2) ** 0.5
+print(hipotenus)
+
+print (a, b, hipotenus)
+
+print(a,b,hipotenus, sep=' ----- >')
+
+print(a)
+print(b)
+print (hipotenus)
+
+print (a, end="bitiiiiiii" )
+print(b)
+print (hipotenus)
+
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+
+print (bir, iki, uc)
+
+print (bir, iki, uc, sep=' ')
+
+print(bir, iki, uc, sep='____bosluk____')
+
+print(bir)
+print(iki)
+print(uc)
+
+print(bir, end = "\n")
+print(iki, end = '\n')
+print(uc)
+
+print (bir, end = "\n**** ")
+print (iki, end = '\n')
+print(uc)  
+
+print (bir, iki, uc, sep="* - *" ,end= '&' )
+print(bir, iki, uc, sep=' ')
+print(bir, iki, uc, sep=' ')
+
+print(bir, iki, uc, sep='* - *', end ='&\n')
+print (bir, iki, uc, sep=' ')
+print(bir, iki, uc, sep=' ')
+
+print('abc', 555)
+
+text1 = "I bought"
+text3 = text1 + 'apple'
+
+text3
+
+text = 'Bu\gun\ben\neden \taksiye\bindim'
+print(text)
+
+print('Bu\gun\\ben\\neden\\taksiye\\bindim')
+
+print (r'Bu\gun\ben\neden\taksiye\bindim')
